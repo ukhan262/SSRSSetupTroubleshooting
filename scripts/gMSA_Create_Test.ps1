eda2c4d21ba94c6b9665335a47ff1fa70651f859
Umar Khan <umarkhan252@gmail.com>
2019-08-22 00:39:04 -0700

    added powershell script for gMSA

---
+++ scripts/gMSA_Create_Test.ps1
@@ -0,0 +1,10 @@
+﻿# Run on Domain Controller to create the gMSA #
+# Make sure the group is already created #
+New-ADServiceAccount accountname -DNSHostName groupNamefortheAccount –PrincipalsAllowedToDelegateToAccount "CN=accountname,CN=folder,DC=parentfolder"
+
+# Add the machines to the group for authenticating #
+# Restart the machines #
+# Test the authentication #
+Test-ADServiceAccount accountname
+
+# True -> Success #
